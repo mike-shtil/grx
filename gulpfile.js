c72370f09a6dd5ac4b79b2f53e7c9855b838c1bd
mike-shtil <mike002@gmail.com>
2016-04-12 02:30:21 +0300

    cleanup of paths and files

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,40 @@
 var gulp = require('gulp');
 
-//build process
-var plumber = require('gulp-plumber');
-var watch = require('gulp-watch');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-//var cssnano = require('gulp-cssnano');
-
+var $ = require('gulp-load-plugins')();
 
 //default task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['demo', 'dist']);
 
 //watch Files For Changes
 gulp.task('watch', function() {
-   gulp.watch('css/**/*.scss', ['sass']);
+  gulp.watch('**/*.scss', ['sass']);
+});
+
+gulp.task('demo', function(){
+  gulp.src('demo/styles/**/*.scss')
+     .pipe($.plumber({errorHandler: function(error){
+        console.log(error);
+        this.emit('end');
+     }}))
+     .pipe($.sourcemaps.init())
+     .pipe($.sass({errLogToConsole: true, sourcemap: true, style: 'compact'}))
+     .pipe($.autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
+     .pipe($.sourcemaps.write({includeContent: true, sourceRoot: '.'}))
+     //.pipe(cssnano({discardDuplicates: false})) //optional minification
+     .pipe(gulp.dest('demo/styles'));
 });
 
 //SASS build process
-gulp.task('sass', function() {
-   return gulp.src('css/**/*.scss')
-      .pipe(plumber({errorHandler: function(error){
+gulp.task('dist', function() {
+   gulp.src('src/**/*.scss')
+      .pipe($.plumber({errorHandler: function(error){
          console.log(error);
          this.emit('end');
       }}))
-      .pipe(sourcemaps.init())
-      .pipe(sass({errLogToConsole: true, sourcemap: true, style: 'compact'}))
-      .pipe(autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
-      .pipe(sourcemaps.write({includeContent: true, sourceRoot: '.'}))
-      .pipe(gulp.dest('css'));
+      .pipe($.sourcemaps.init())
+      .pipe($.sass({errLogToConsole: true, sourcemap: true, style: 'compact'}))
+      .pipe($.autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
+      .pipe($.sourcemaps.write({includeContent: true, sourceRoot: '.'}))
       //.pipe(cssnano({discardDuplicates: false})) //optional minification
+      .pipe(gulp.dest('dist'));
 });

f04e8615f1efc476abd151a74c5581189bc6fe3c
mike-shtil <mike002@gmail.com>
2016-09-14 22:51:00 +0300

    removed demo page; separated parameters to its own file

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,11 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 //default task
-gulp.task('default', ['dist', 'demo', 'watch']);
+gulp.task('default', ['dist', 'watch']);
 
 //watch Files For Changes
 gulp.task('watch', function() {
-  return gulp.watch('**/*.scss', ['dist', 'demo']);
-});
-
-gulp.task('demo', function(){
-  return gulp.src('demo/style/**/*.scss')
-     .pipe($.plumber({errorHandler: function(error){
-        console.log(error);
-        this.emit('end');
-     }}))
-     .pipe($.sourcemaps.init())
-     .pipe($.sass({errLogToConsole: true, sourcemap: true, style: 'compact'}))
-     .pipe($.autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false}))
-     .pipe($.sourcemaps.write({includeContent: false, sourceRoot: '.'}))
-     //.pipe($.cssnano({discardDuplicates: false})) //optional minification
-     .pipe(gulp.dest('demo/style'));
+  return gulp.watch('**/*.scss', ['dist']);
 });
 
 //SASS build process
